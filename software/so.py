e71e37d754d4349634d4ed0512f763614a5eb648
gabriellybc <gabriellycariman@gmail.com>
2023-05-18 10:09:40 -0300

    muita doidera

--- software/so.py
+++ software/so.py
@@ -8,10 +8,13 @@ def run_python_program():
     resultado = subprocess.run(comando, capture_output=True, text=True)
 
     # Verifica se a execução foi bem-sucedida
-    if resultado.returncode == 0:
+    if resultado.returncode == 1:
+        print("Abrir")
         print("Programa executado com sucesso.")
         print("Saída do programa:")
         print(resultado.stdout)
+    elif resultado.returncode == 2:
+        print("Fechar")
     else:
         print("Ocorreu um erro durante a execução do programa.")
         print("Erro:")
--- software/telegram_bot.py
+++ software/telegram_bot.py
@@ -1,11 +1,16 @@
 import logging
 import asyncio
+import threading
+import os
+
 from telegram import Update, Bot, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler, Updater
 
 TOKEN = '5813859429:AAH1KS33INDbN1LMa2SDALBH53WngdU2aJk'
-
+CHAT_ID = '806031627'
 reports = {}
+
+application = ApplicationBuilder().token(TOKEN).build()
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -19,50 +24,44 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text=f"{name}, você está cadastrado(a) no sistema!"
     await context.bot.send_message(chat_id=chat_id, text=text)
 
+async def sim(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reports["open_gate"] = True
+    text=f"Você mandou SIM. O portão será ABERTA"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    os._exit(1)
+
+async def nao(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reports["open_gate"] = False
+    text=f"Você mandou NÃO. O portão ficará FECHADA"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    os._exit(2)
+
 async def send_message(chat_id):
-    # Crie uma instância do bot
     bot = Bot(token=TOKEN)
     
-    mensagem = "Essa pessoa deseja entrar. Você quer abriar a porta?"
-    photo_path = "unknown_face/data.png"  # Insira o caminho completo da foto
-
-    keyboard = [
-        [InlineKeyboardButton("Sim", callback_data='sim')],
-        [InlineKeyboardButton("Não", callback_data='nao')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    # Envie a foto para o chat
+    photo_path = "unknown_face/data.png" 
     await bot.send_photo(chat_id=chat_id, photo=open(photo_path, 'rb'))
 
-    # Envie a mensagem para o chat
+    mensagem = "Essa pessoa deseja entrar. Você permite a sua entrada?\n\n- Mande /sim para ABRIR portão\n- Mande /nao para manter o portão FECHADO"
     await bot.send_message(chat_id=chat_id, text=mensagem)
 
+def my_thread():
+    asyncio.run(send_message(CHAT_ID))
 
-async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    user_id = query.from_user.id
-    response = query.data
-
-    # Salve a resposta do usuário
-    reports[user_id] = response
-
-    await context.bot.send_message(chat_id=user_id, text=f"Você escolheu: {response}")
-
-if __name__ == '__main__':
-
-    asyncio.run(send_message("806031627"))
-
-    application = ApplicationBuilder().token(TOKEN).build()
+def telegram_bot():
+    thread = threading.Thread(target=my_thread)
+    thread.start()
+    thread.join()
 
     start_handler = CommandHandler('start', start)
-    button_handler = CallbackQueryHandler(button_callback)
+    sim_handler = CommandHandler('sim', sim)
+    nao_handler = CommandHandler('nao', nao)
 
     application.add_handler(start_handler)
-    application.add_handler(button_handler)
-
+    application.add_handler(sim_handler)
+    application.add_handler(nao_handler)
+        
     application.run_polling()
 
-    
-
-    
+if __name__ == '__main__':
+    telegram_bot()
