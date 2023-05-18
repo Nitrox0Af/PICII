a0c1cc8b9f9f1f81254f35f2dd88fec0ee3a8f95
gabriellybc <gabriellycariman@gmail.com>
2023-05-18 17:59:43 -0300

    v1 - add telegram to raspberry

--- software/main.py
+++ software/main.py
@@ -1,5 +1,7 @@
 import os
 import time
+import subprocess
+
 import numpy as np
 import cv2
 import face_recognition
@@ -9,6 +11,7 @@ def main():
     known_faces_dir = "known_faces"
     known_faces = save_encodings_dict(known_faces_dir)
     recognize_unknown(known_faces, tolerance=0.6)
+
 
 def save_encodings_dict(known_faces_dir: str) -> dict:
     """Runs through all folders and images to save the generated encodings"""
@@ -30,15 +33,17 @@ def save_encodings_dict(known_faces_dir: str) -> dict:
             continue
         encoding = encodings[0]
         save_encoding(person_dir, filename, encoding)
-#         encoding = load_encoding(person_dir, filename)
+        # encoding = load_encoding(person_dir, filename)
         known_faces[person_identifier].append(encoding)
     return known_faces
+
 
 def generate_encodings(path: str) -> list:
     """Generates encodings from a photo"""
     image = face_recognition.load_image_file(path, mode='RGB')
     encodings = face_recognition.face_encodings(image)
     return encodings
+
 
 def save_encoding(path: str, filename: str, encoding):
     """Saves encoding in pickle format"""
@@ -54,13 +59,13 @@ def load_encoding(path: str, filename: str):
     encoding = pickle.load(open(path_file, 'rb'))
     return encoding
 
+
 def recognize_unknown(known_faces: dict, tolerance: float = 0.6):
     """Takes a photo and recognizes the unknown person"""
     while True:
         print("Take the picture of the unknown person")
         img_name = "unknown_face/data.png"
         key = input()
-        key = "COMP"
         if key == "ESC":
             print("EXIT")
             break
@@ -88,20 +93,41 @@ def recognize_unknown(known_faces: dict, tolerance: float = 0.6):
 
                 for identifier, face_encodings in known_faces.items():
                     matches = face_recognition.compare_faces(face_encodings, unknown_face_encoding, tolerance=tolerance)
+
                     if True in matches:
                         found_match = True
                         print(f"This is the image of {identifier}!")
                         average_distance = np.average(face_recognition.face_distance(face_encodings, unknown_face_encoding))
                         print(f"Has an average distance of {round(average_distance, 3)}")
+
+                        print("Opening Telegram Bot")
+                        run_telegram_bot(identifier)
                         break
 
                 if not found_match:
                     print("This person is not recognized")
+
+                    print("Opening Telegram Bot")
+                    run_telegram_bot("unknown")
+
             print("Press ESC to exit or COMP to take photo")
 
         else:
             print("Invalid command")
             print("Press ESC to exit or COMP to take photo")
+
+
+def run_telegram_bot(person):
+    """RunS Telegram Bot to send the photo of the person you want to enter. Ask the owner if you can open the gate or not"""
+    command = ["python", "telegram_bot.py", person]
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 1:
+        print("Abrir Portão")
+    elif result.returncode == 2:
+        print("Manter Portão Fechado")
+    else:
+        print("Error:")
+        print(result.stderr)
 
 
 if __name__ == "__main__":
--- software/telegram_bot.py
+++ software/telegram_bot.py
@@ -2,53 +2,26 @@ import logging
 import asyncio
 import threading
 import os
+import sys
 
-from telegram import Update, Bot, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler, Updater
+from telegram import Update, Bot
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
 
 TOKEN = '5813859429:AAH1KS33INDbN1LMa2SDALBH53WngdU2aJk'
 CHAT_ID = '806031627'
+args = sys.argv
 reports = {}
-
-application = ApplicationBuilder().token(TOKEN).build()
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    name = update.message.from_user.first_name
-    chat_id = update.effective_chat.id
-    reports["chat_id"] = chat_id
-    text=f"{name}, você está cadastrado(a) no sistema!"
-    await context.bot.send_message(chat_id=chat_id, text=text)
+application = ApplicationBuilder().token(TOKEN).build()
 
-async def sim(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    reports["open_gate"] = True
-    text=f"Você mandou SIM. O portão será ABERTA"
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-    os._exit(1)
 
-async def nao(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    reports["open_gate"] = False
-    text=f"Você mandou NÃO. O portão ficará FECHADA"
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-    os._exit(2)
-
-async def send_message(chat_id):
-    bot = Bot(token=TOKEN)
-    
-    photo_path = "unknown_face/data.png" 
-    await bot.send_photo(chat_id=chat_id, photo=open(photo_path, 'rb'))
-
-    mensagem = "Essa pessoa deseja entrar. Você permite a sua entrada?\n\n- Mande /sim para ABRIR portão\n- Mande /nao para manter o portão FECHADO"
-    await bot.send_message(chat_id=chat_id, text=mensagem)
-
-def my_thread():
-    asyncio.run(send_message(CHAT_ID))
-
-def telegram_bot():
+def main():
     thread = threading.Thread(target=my_thread)
     thread.start()
     thread.join()
@@ -63,5 +36,42 @@ def telegram_bot():
         
     application.run_polling()
 
+
+def my_thread():
+    person = args[1]
+    asyncio.run(send_message(CHAT_ID, person))
+
+
+async def send_message(chat_id: str, person: str):
+    bot = Bot(token=TOKEN)
+    
+    photo_path = "unknown_face/data.png" 
+    await bot.send_photo(chat_id=chat_id, photo=open(photo_path, 'rb'))
+
+    person = "Essa pessoa" if person == "unknown" else person
+    mensagem = f"{person} deseja entrar. Você permite a sua entrada?\n\n- Mande /sim para ABRIR portão\n- Mande /nao para manter o portão FECHADO"
+    await bot.send_message(chat_id=chat_id, text=mensagem)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    name = update.message.from_user.first_name
+    chat_id = update.effective_chat.id
+    reports["chat_id"] = chat_id
+    text=f"{name}, você está cadastrado(a) no sistema!"
+    await context.bot.send_message(chat_id=chat_id, text=text)
+
+
+async def sim(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text=f"Você mandou SIM. O portão será ABERTO"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    os._exit(1)
+
+
+async def nao(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text=f"Você mandou NÃO. O portão ficará FECHADO"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    os._exit(2)
+
+
 if __name__ == '__main__':
-    telegram_bot()
+    main()
