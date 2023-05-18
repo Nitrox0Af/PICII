5298b0e7aa88fed3c92eb308e30f47468239889f
gabriellybc <gabriellycariman@gmail.com>
2023-05-18 00:41:17 -0300

    testes telegram

--- software/telegram_bot.py
+++ software/telegram_bot.py
@@ -1,12 +1,7 @@
 import logging
 import asyncio
 from telegram import Update, Bot, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import (
-    ApplicationBuilder,
-    ContextTypes,
-    CommandHandler,
-    CallbackQueryHandler,
-)
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler
 
 TOKEN = '5813859429:AAH1KS33INDbN1LMa2SDALBH53WngdU2aJk'
 
@@ -21,16 +16,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     name = update.message.from_user.first_name
     chat_id = update.effective_chat.id
     reports["chat_id"] = chat_id
-    text = f"{name}, você está cadastrado(a) no sistema!"
-
-    # Criação dos botões "Sim" e "Não"
-    keyboard = [
-        [InlineKeyboardButton("Sim", callback_data="sim"),
-         InlineKeyboardButton("Não", callback_data="nao")]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await context.bot.send_message(chat_id=chat_id, text=text, reply_markup=reply_markup)
+    text=f"{name}, você está cadastrado(a) no sistema!"
+    await context.bot.send_message(chat_id=chat_id, text=text)
 
 async def send_message(chat_id):
     # Crie uma instância do bot
@@ -52,25 +39,23 @@ async def send_message(chat_id):
     await bot.send_message(chat_id=chat_id, text=mensagem)
 
 
-async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    chat_id = query.message.chat_id
-    data = query.data
+    user_id = query.from_user.id
+    response = query.data
 
-    if data == "sim":
-        await context.bot.send_message(chat_id=chat_id, text="Você escolheu 'Sim'!")
-    elif data == "nao":
-        await context.bot.send_message(chat_id=chat_id, text="Você escolheu 'Não'!")
+    # Salve a resposta do usuário
+    reports[user_id] = response
+
+    await context.bot.send_message(chat_id=user_id, text=f"Você escolheu: {response}")
 
 if __name__ == '__main__':
-
     application = ApplicationBuilder().token(TOKEN).build()
 
     start_handler = CommandHandler('start', start)
-    button_handler = CallbackQueryHandler(button_click)
+    button_handler = CallbackQueryHandler(button_callback)
 
     application.add_handler(start_handler)
-    
     application.add_handler(button_handler)
 
     application.run_polling()
--- software/test.py
+++
@@ -1,3 +0,0 @@
-from numpy import genfromtxt
-my_data = genfromtxt('Encodings/Mayke.csv', delimiter=',')
-print(type(my_data))
