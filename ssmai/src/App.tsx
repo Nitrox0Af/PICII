2f74d7128a0f73b0424f16f61733b44f7f99c024
Nitrox0Af <mayke.ace@hotmail.com>
2023-05-22 03:47:11 -0300

    frontend

--- ssmai/src/App.tsx
+++ ssmai/src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './Styles/Global.css';
+import './Styles/Styles.css';
+import React from 'react'
+import ThumbnailUploader from './Components/Thumbnailuploader';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='infos'>
+      <h1 className='Uploader'>
+        <ThumbnailUploader />
+      </h1>
+      <h1 className='n1'>
+        CPF*:
+      </h1>
+      <h1 className='n2'>
+        Nome*:
+      </h1>
+      <h1 className='n3'>
+        Apelido*:
+      </h1>
+      <h1 className='n4'>
+        Parentesco*:
+      </h1>
+      <input className='cpf' placeholder='000.000.000-00'></input>
+      <input className='name' placeholder='fulano'></input>
+      <input className='surname' placeholder='ciclano'></input>
+      <input className='relationship' placeholder='primo'></input>
+   </div>
   );
 }
 
