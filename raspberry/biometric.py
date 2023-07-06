2c84c163892855708d66567fa6a9a9c2fc8c3079
gabriellybc <gabriellycariman@gmail.com>
2023-07-06 18:35:14 -0300

    teste

--- raspberry/biometric.py
+++ raspberry/biometric.py
@@ -1,59 +1,33 @@
-import fingerprinth
+from pyfingerprint import PyFingerprint
 
-class BiometricModule:
-    def __init__(self):
-        self.sensor = fingerprinth.Fingerprint()
-    
-    def connect(self):
-        """Connect to the biometric module."""
-        try:
-            self.sensor.open()
-        except fingerprinth.FingerprintException as e:
-            print(f"Failed to connect to the biometric module: {e}")
+# Inicialização do leitor de impressões digitais
+try:
+    fingerprint = PyFingerprint('/dev/ttyAMA0', 57600, 0xFFFFFFFF, 0x00000000)
+    if not fingerprint.verifyPassword():
+        raise ValueError('Senha incorreta')
 
-    def enroll(self, fingerprint_id):
-        """Enroll a new fingerprint with the given ID."""
-        try:
-            self.sensor.enroll(fingerprint_id)
-            print(f"Fingerprint enrolled successfully with ID: {fingerprint_id}")
-        except fingerprinth.FingerprintException as e:
-            print(f"Failed to enroll fingerprint: {e}")
-    
-    def verify(self):
-        """Verify the fingerprint."""
-        try:
-            fingerprint_id = self.sensor.capture()
-            if fingerprint_id >= 0:
-                print(f"Fingerprint verified successfully with ID: {fingerprint_id}")
-            else:
-                print("Fingerprint verification failed")
-        except fingerprinth.FingerprintException as e:
-            print(f"Failed to verify fingerprint: {e}")
+except Exception as e:
+    print(f'Erro ao inicializar o leitor de impressões digitais: {e}')
+    exit(1)
 
-    def delete(self, fingerprint_id):
-        """Delete the fingerprint with the given ID."""
-        try:
-            self.sensor.delete(fingerprint_id)
-            print(f"Fingerprint with ID {fingerprint_id} deleted successfully")
-        except fingerprinth.FingerprintException as e:
-            print(f"Failed to delete fingerprint: {e}")
+# Verificação de uma impressão digital
+try:
+    print('Coloque o dedo no leitor...')
+    while not fingerprint.readImage():
+        pass
 
-    def disconnect(self):
-        """Disconnect from the biometric module."""
-        self.sensor.close()
+    fingerprint.convertImage(0x01)
+    result = fingerprint.searchTemplate()
+    position = result[0]
 
-def main():
-    biometric_module = BiometricModule()
-    biometric_module.connect()
+    if position >= 0:
+        print(f'Impressão digital encontrada na posição #{position}')
+    else:
+        print('Impressão digital não encontrada')
 
-    try:
-        # Use the biometric module
-        biometric_module.enroll(1)
-        biometric_module.verify()
-        biometric_module.delete(1)
+except Exception as e:
+    print(f'Erro na verificação da impressão digital: {e}')
+    exit(1)
 
-    except KeyboardInterrupt:
-        biometric_module.disconnect()
-
-if __name__ == '__main__':
-    main()
+# Limpeza
+fingerprint.close()
