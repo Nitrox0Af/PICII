7f5ffe6871ab60aa4712ace3bd287c589e2e79fb
gabriellybc <gabriellycariman@gmail.com>
2023-07-06 17:03:06 -0300

    add biometria

--- raspberry/biometric.py
+++ raspberry/biometric.py
@@ -10,43 +10,50 @@ class Biometric:
     def connect(self):
         """Connect to the biometric module."""
         self.serial = serial.Serial(self.port, baudrate=9600, timeout=1)
-        time.sleep(2) 
+        time.sleep(2)  # Tempo de espera para a inicialização do módulo
 
     def enroll_fingerprint(self):
         """Enroll a new fingerprint."""
-        print("Coloque o dedo no módulo...")
-        while True:
-            response = self.serial.readline().strip().decode()
-            if response == "OK":
-                print("Fingerprint enrolled successfully!")
-                break
-            elif response == "ERROR":
-                print("Error enrolling fingerprint!")
-                break
+        self.serial.write(b"ENROLL\n")
+        response = self.serial.readline().strip().decode()
+        if response == "READY":
+            print("Coloque o dedo no módulo...")
+            while True:
+                response = self.serial.readline().strip().decode()
+                if response == "OK":
+                    print("Fingerprint enrolled successfully!")
+                    break
+                elif response == "ERROR":
+                    print("Error enrolling fingerprint!")
+                    break
 
     def verify_fingerprint(self):
         """Verify a fingerprint."""
-        print("Coloque o dedo no módulo...")
-        while True:
-            response = self.serial.readline().strip().decode()
-            if response == "MATCH":
-                print("Fingerprint verified successfully!")
-                break
-            elif response == "NO MATCH":
-                print("Fingerprint not verified!")
-                break
+        self.serial.write(b"VERIFY\n")
+        response = self.serial.readline().strip().decode()
+        if response == "READY":
+            print("Coloque o dedo no módulo...")
+            while True:
+                response = self.serial.readline().strip().decode()
+                if response == "MATCH":
+                    print("Fingerprint verified successfully!")
+                    break
+                elif response == "NO MATCH":
+                    print("Fingerprint not verified!")
+                    break
 
     def disconnect(self):
         """Disconnect from the biometric module."""
         self.serial.close()
 
 def main():
-    biometric = Biometric(BIOMETRIC_PORT)
+    biometric = Biometric(BIOMETRIC_PORT)  # Insira a porta correta do módulo
     biometric.connect()
 
     try:
         biometric.enroll_fingerprint()
 
+        # Faça a verificação de impressão digital apenas após a impressão digital ser registrada
         biometric.verify_fingerprint()
 
     except Exception as e:
