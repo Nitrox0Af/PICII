7c72423c4648ac5a16c00d57f5a0b36d1e4590de
gabriellybc <gabriellycariman@gmail.com>
2023-07-11 17:01:48 -0300

    teste

--- raspberry/keyboard2.py
+++ raspberry/keyboard2.py
@@ -1,33 +1,35 @@
-from machine import Pin
-import utime
+import RPi.GPIO as GPIO
+import time
 
-row_list = [26, 20, 19, 16]  
+row_list = [26, 20, 19, 16]
 col_list = [13, 6, 5]
 
+GPIO.setmode(GPIO.BCM)
+
 for x in range(0, 4):
-  row_list[x] = Pin(row_list[x], Pin.OUT)
-  row_list[x].value(1)
+    GPIO.setup(row_list[x], GPIO.OUT)
+    GPIO.output(row_list[x], GPIO.HIGH)
 
-for x in range(0 ,3):
-  col_list[x] = Pin(col_list[x], Pin.IN, Pin.PULL_UP)
+for x in range(0, 3):
+    GPIO.setup(col_list[x], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-key_list = [["1", "2", "3"],\
-      ["4", "5", "6"],\
-      ["7", "8", "9"],\
-      ["*", "0", "#"]]
+key_list = [["1", "2", "3"],
+            ["4", "5", "6"],
+            ["7", "8", "9"],
+            ["*", "0", "#"]]
 
 def keypad(col, row):
-  for r in row:
-    r.value(0)
-    result = [col[0].value(), col[1].value(), col[2].value()]
-    if min(result) == 0:
-      key = key_list[int(row.index(r))][int(result.index(0))]
-      r.value(1)
-      return (key)
-    r.value(1)
+    for r in row:
+        GPIO.output(r, GPIO.LOW)
+        result = [GPIO.input(col[0]), GPIO.input(col[1]), GPIO.input(col[2])]
+        if min(result) == 0:
+            key = key_list[int(row.index(r))][int(result.index(0))]
+            GPIO.output(r, GPIO.HIGH)
+            return key
+        GPIO.output(r, GPIO.HIGH)
 
 while True:
-  key = keypad(col_list, row_list)
-  if key != None:
-    print("key: "+key)
-    utime.sleep(0.3)
+    key = keypad(col_list, row_list)
+    if key is not None:
+        print("Key: " + key)
+        time.sleep(0.3)
