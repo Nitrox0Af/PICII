229699287cffc83c40be1b3c436cba982d8ec1e9
gabriellybc <gabriellycariman@gmail.com>
2023-07-11 13:50:13 -0300

    TESTE

--- raspberry/button3.py
+++ raspberry/button3.py
@@ -1,7 +1,18 @@
-from gpiozero import Button
-from config import BUTTON_PIN
+from gpiozero import Button, LED
+from config import BUTTON_PIN, LED_PIN
 
+# button = Button(BUTTON_PIN)
+
+# button.wait_for_press()
+# print('You pushed me')
+
+# from gpiozero import LED, Button
+from signal import pause
+
+led = LED(LED_PIN)
 button = Button(BUTTON_PIN)
 
-button.wait_for_press()
-print('You pushed me')
+button.when_pressed = led.on
+button.when_released = led.off
+
+pause()
