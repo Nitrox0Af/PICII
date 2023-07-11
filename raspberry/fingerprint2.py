fe71a42ddcd81faccc08dfab130a5a367b266db5
gabriellybc <gabriellycariman@gmail.com>
2023-07-11 15:04:33 -0300

    vaii

--- raspberry/fingerprint2.py
+++ raspberry/fingerprint2.py
@@ -3,7 +3,7 @@ from config import FINGERPRINT_PORT
 import serial
 
 print("Inicializando sensor de impressão digital...")
-uart = serial.Serial(FINGERPRINT_PORT, baudrate=57600, timeout=1)
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 print("Criando objeto de sensor de impressão digital...")
 finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
