0874a7aa900e7c2a9fb59b37cafdf64c0dc12e08
gabriellybc <gabriellycariman@gmail.com>
2023-07-11 15:00:53 -0300

    mais um

--- raspberry/fingerprint2.py
+++ raspberry/fingerprint2.py
@@ -1,6 +1,6 @@
 import adafruit_fingerprint
 from config import FINGERPRINT_PORT
 import serial
-uart = serial.Serial("/dev/ttyS0", baudrate=57600, timeout=1)
+uart = serial.Serial(FINGERPRINT_PORT, baudrate=57600, timeout=1)
 
 finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
