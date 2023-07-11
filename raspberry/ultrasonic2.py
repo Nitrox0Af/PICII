f5f5a4acd7148a2c714d8afc32c75d1b75d79f63
gabriellybc <gabriellycariman@gmail.com>
2023-07-11 14:18:32 -0300

    melhoria ultra

--- raspberry/ultrasonic2.py
+++ raspberry/ultrasonic2.py
@@ -1,5 +1,17 @@
 from gpiozero import DistanceSensor
 from config import TRIG_PIN, ECHO_PIN
-ultrasonic = DistanceSensor(echo=ECHO_PIN, trigger=TRIG_PIN)
+
+def hello():
+    print("Hello")
+
+def bye():
+    print("Bye")
+
+ultrasonic = DistanceSensor(echo=ECHO_PIN, trigger=TRIG_PIN, threshold_distance=0.5, max_distance=2)
+
+ultrasonic.when_in_range = hello
+
+ultrasonic.when_out_of_range = bye
+
 while True:
     print(ultrasonic.distance)
