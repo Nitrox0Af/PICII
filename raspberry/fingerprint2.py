2b5e894681d5167c039bb18cf668c91074daa770
gabriellybc <gabriellycariman@gmail.com>
2023-07-11 14:50:44 -0300

    fix

--- raspberry/fingerprint2.py
+++ raspberry/fingerprint2.py
@@ -2,8 +2,8 @@ import adafruit_fingerprint
 from config import FINGERPRINT_PORT
 import serial
 
-uart = serial.Serial("/dev/ttyS0", baudrate=57600, timeout=1)
+vai = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 print("OK")
-finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
+finger = adafruit_fingerprint.Adafruit_Fingerprint(vai)
 print("Waiting for fingerprint ...")
