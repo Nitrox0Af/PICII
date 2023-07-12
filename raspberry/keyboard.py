2435c2706f0b4045ad7f996f4563087309957737
gabriellybc <gabriellycariman@gmail.com>
2023-07-11 22:53:10 -0300

    teclado

--- raspberry/keyboard.py
+++ raspberry/keyboard.py
@@ -1,29 +1,23 @@
 import RPi.GPIO as GPIO
 import time
+from config import ROW_PINS, COL_PINS, KEY_MATRIX
 
-row_list = [26, 20, 19, 16]
-col_list = [6, 13, 5]
+def setup():
+    GPIO.setmode(GPIO.BCM)
 
-GPIO.setmode(GPIO.BCM)
+    for x in range(0, 4):
+        GPIO.setup(ROW_PINS[x], GPIO.OUT)
+        GPIO.output(ROW_PINS[x], GPIO.HIGH)
 
-for x in range(0, 4):
-    GPIO.setup(row_list[x], GPIO.OUT)
-    GPIO.output(row_list[x], GPIO.HIGH)
-
-for x in range(0, 3):
-    GPIO.setup(col_list[x], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-key_list = [["1", "2", "3"],
-            ["4", "5", "6"],
-            ["7", "8", "9"],
-            ["*", "0", "#"]]
+    for x in range(0, 3):
+        GPIO.setup(COL_PINS[x], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def keypad(col, row):
     for r in row:
         GPIO.output(r, GPIO.LOW)
         result = [GPIO.input(col[0]), GPIO.input(col[1]), GPIO.input(col[2])]
         if min(result) == 0:
-            key = key_list[int(row.index(r))][int(result.index(0))]
+            key = KEY_MATRIX[int(row.index(r))][int(result.index(0))]
             GPIO.output(r, GPIO.HIGH)
             return key
         GPIO.output(r, GPIO.HIGH)
@@ -31,11 +25,13 @@ def keypad(col, row):
 def main():
     try:
         while True:
-            key = keypad(col_list, row_list)
+            key = keypad(COL_PINS, ROW_PINS)
             if key is not None:
-                print("Key: " + key)
                 return key
     except KeyboardInterrupt:
         GPIO.cleanup()
 
-main()
+if __name__ == '__main__':
+    setup()
+    key = main()
+    print(key)
