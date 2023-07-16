b495db8f18787e29f644c55d28485d4740eccf94
Nitrox0Af <mayke.ace@hotmail.com>
2023-07-16 15:00:47 -0300

    porta

--- raspberry/main.py
+++ raspberry/main.py
@@ -7,7 +7,7 @@ import recognizer
 import keyboard
 import reed
 from gpiozero import DistanceSensor, LED, Buzzer, Button
-from config import TRIG_PIN, ECHO_PIN, MAX_DISTANCE, MIN_DISTANCE, QNTD_RECOGNIZE, WAITING_TIME, SYSTEM_PASSWORD, RED_LED_PIN, GREEN_LED_PIN, TIME_BLINK, BUZZER_PIN, BUTTON_PIN, DELAY_REED, OPEN_TIME, DEBOUNCE_TIME
+from config import TRIG_PIN, ECHO_PIN, MAX_DISTANCE,GATE , MIN_DISTANCE, QNTD_RECOGNIZE, WAITING_TIME, SYSTEM_PASSWORD, RED_LED_PIN, GREEN_LED_PIN, TIME_BLINK, BUZZER_PIN, BUTTON_PIN, DELAY_REED, OPEN_TIME, DEBOUNCE_TIME
 
 
 OPEN = False
@@ -22,7 +22,7 @@ led_red = LED(RED_LED_PIN)
 led_green = LED(GREEN_LED_PIN)
 buzzer = Buzzer(BUZZER_PIN)
 reed.setup()
-
+gate_out = GPIO.output(GATE)
 
 def main():
     """Main function."""
@@ -61,36 +61,49 @@ def display():
     print("\n")
     print("\n")
 
+
 def recognizer_face():
+    time_start = time.time()
     have_person = 0
-    distance = round(ultrasonic.distance * 100)
+    while True:
+        distance = round(ultrasonic.distance * 100)
 
-    if distance > MAX_DISTANCE:
-        print(f"\nAproxime-se do sensor.\nA distância máxima é {MAX_DISTANCE}cm. \nDistancia atual: {distance}cm\n\n")
-        time.sleep(WAITING_TIME)
-        have_person = 0
-    elif distance < MIN_DISTANCE:
-        print(f"\nAfaste-se do sensor. \nA distância minima é {MIN_DISTANCE}cm. \nDistancia atual: {distance}cm\n\n")
-        time.sleep(WAITING_TIME)
-    elif distance <= MAX_DISTANCE:
-        have_person += 1
-        print(f"\nPerfeito! Você foi detectado(a) a {distance}cm")
-    
-    if have_person >= QNTD_RECOGNIZE:
-        have_person = 0
+        if time.time() - time_start > WAITING_TIME*2:
+            print("\nTempo de espera esgotado!")
+            not_open_gate()
+            break
 
-        print("\nIniciando processo de tirar foto...")
-        take_photo = photo_capture.main()
+        if distance > MAX_DISTANCE:
+            print(f"\nAproxime-se do sensor.\nA distância máxima é {MAX_DISTANCE}cm. \nDistancia atual: {distance}cm\n\n")
+            time.sleep(WAITING_TIME)
+            have_person = 0
+        elif distance < MIN_DISTANCE:
+            print(f"\nAfaste-se do sensor. \nA distância minima é {MIN_DISTANCE}cm. \nDistancia atual: {distance}cm\n\n")
+            time.sleep(WAITING_TIME)
+        elif distance <= MAX_DISTANCE:
+            have_person += 1
+            print(f"\nPerfeito! Você detectado(a) a {distance}cm")
+            time.sleep(WAITING_TIME)
+            time_start = time.time()
         
-        if take_photo:
-            print("\nIniciando processo de reconhecimento...")
-            open_gate = recognizer.main()
-            if open_gate:
-                    print("\nAbrir Portão!")
-                    open_gate()
-            else:
-                print("\nNão Abrir!")
-                not_open_gate()
+        if have_person >= QNTD_RECOGNIZE:
+            os.system('clear')
+            have_person = 0
+
+            print("\nIniciando processo de tirar foto...")
+            take_photo = photo_capture.main()
+            
+            if take_photo:
+                print("\nIniciando processo de reconhecimento...")
+                open_gate = recognizer.main()
+                if open_gate:
+                        print("\nAbrir Portão!")
+                        open_gate()
+                        break
+                else:
+                    print("\nNão Abrir!")
+                    not_open_gate()
+                    break
 
 
 def password():
@@ -110,11 +123,11 @@ def password():
     
     if characters == SYSTEM_PASSWORD:
         characters = ""
-        print("Senha certa!")
+        print("Correct password!")
         open_gate()
     else:
         characters = ""
-        print("Senha errada!")
+        print("Wrong password!")
         not_open_gate()
 
 
@@ -125,29 +138,27 @@ def close_gate():
         if not reed.get_input():
             if (time.time() - time_of_change) >= OPEN_TIME:
                 print("\nFechar Portão!")
-                blink_led_buzzer(led_red)
-                break
-        else:
-            print("\nArguardando o Potão ser Fechado!")
-            blink_led(led_green)
+                blink_led(led_red)
+                    
+        time.sleep(DELAY_REED)
 
 
 def open_gate():
     """Open gate"""
     if reed.get_input():
         print("\nAbrir Portão!")
-        blink_led(led_green)
+        blink_led_buzzer(led_green)
         close_gate()
         display()
     else:
         print("\nPortão já está aberto!")
-        blink_led(led_red)
 
 
 def not_open_gate():
     """Not open gate"""
     print("\nNão Abrir!")
     blink_led_buzzer(led_red)
+    display()
 
 
 def blink_led(led):
